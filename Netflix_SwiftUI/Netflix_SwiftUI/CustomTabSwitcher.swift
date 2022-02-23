08dc13923aae6ae76c115319ebff8d2e7dc2de90
ejdgudl <>
2022-02-23 20:46:36 +0900

    13. Custom Tab Switcher

--- Netflix_SwiftUI/Netflix_SwiftUI/CustomTabSwitcher.swift
+++ Netflix_SwiftUI/Netflix_SwiftUI/CustomTabSwitcher.swift
@@ -8,27 +8,40 @@
 import SwiftUI
 
 struct CustomTabSwitcher: View {
+    @State private var currentTab: CustomTab = .episodes
     var tabs: [CustomTab]
     
     var body: some View {
         VStack {
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
+                HStack(spacing: 20) {
                     ForEach(tabs, id: \.self) { tab in
                         VStack {
                             Rectangle()
                                 .frame(width: widthForTab(tab), height: 6)
+                                .foregroundColor(tab == currentTab ? .red : .clear)
                             Button {
-                                
+                                currentTab = tab
                             } label: {
                                 Text(tab.rawValue)
                                     .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(tab == currentTab ? .white : .gray)
                             }
+                            .frame(width: widthForTab(tab), height: 30)
                         }
                     }
                 }
             }
-            Text("SELECTED VIEW")
+            switch currentTab {
+            case .episodes:
+                SmallVerticalButton(text: "TEST", isOnImage: "", isOffImage: "", isOn: true) {
+                    
+                }
+            case .trailers:
+                Text("Trailers")
+            case .more:
+                Text("MORE")
+            }
         }
         .foregroundColor(.white)
     }

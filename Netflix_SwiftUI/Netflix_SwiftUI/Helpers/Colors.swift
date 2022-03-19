a0a147a03e6cb793f75045292f922fc4c3fb1305
ejdgudl <>
2022-03-19 10:00:45 +0900

    24. Custom Search Bar (Part 1)

---
+++ Netflix_SwiftUI/Netflix_SwiftUI/Helpers/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  Netflix_SwiftUI
+//
+//  Created by 김동현 on 2022/03/19.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let graySearchBackground = Color("gray_search_background")
+    static let graySearchText = Color("gray_search_text")
+}
